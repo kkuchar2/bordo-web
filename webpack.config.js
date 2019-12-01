d7a7af7c677fd081dfdba895f4bb2361c8ecaad5
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2019-12-01 21:45:53 +0100

    Build to root directory for github-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: __dirname,
         filename: 'bundle.js',
     },
     resolve: {
