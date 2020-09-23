0dafac1a8cb4d4263702acfe84f072e988c50bd0
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2020-09-23 15:00:11 +0200

    Switch to BrowserRouter and add production config for deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import exactIndexRoutes from "routes/exactRoutes.jsx";
 import NotFound from "../src/pages/NotFound.jsx";
@@ -9,12 +9,12 @@ import "./index.scss"
 
 ReactDOM.render(
     <div className="commonPage">
-        <HashRouter>
+        <BrowserRouter>
             <Switch>
                 {exactIndexRoutes.map((p, k) => <Route exact path={p.path} component={p.component} key={k}/>)}
                 <Route component={NotFound} key={0}/>
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     </div>,
     document.getElementById('root')
 );
