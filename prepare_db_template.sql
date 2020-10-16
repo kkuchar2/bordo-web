e15955cafa21475b1b90127c53cda3410c8f7c05
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2020-10-16 02:34:00 +0200

    Delete prepare_db_template.sql

--- prepare_db_template.sql
+++
@@ -1,9 +0,0 @@
-SET GLOBAL validate_password.policy = 'LOW';
-
-CREATE DATABASE IF NOT EXISTS ${db_name};
-CREATE USER IF NOT EXISTS '${username}'@'localhost' IDENTIFIED BY '${password}';
-GRANT ALL PRIVILEGES ON ${db_name}.* TO '${username}'@'localhost';
-FLUSH PRIVILEGES;
-
-SHOW DATABASES;
-SHOW GRANTS FOR '${username}'@'localhost';
