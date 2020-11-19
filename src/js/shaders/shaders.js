54aefd400800f2d3243bfc3b85a063fe27473b5a
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2020-11-19 02:08:05 +0100

    Remove uvs from fragment shader

--- src/js/shaders/shaders.js
+++ src/js/shaders/shaders.js
@@ -7,9 +7,7 @@ export const vertexShader = `
 export const fragmentShader = `
     uniform vec3 color;
     uniform float alpha;
-    
-    varying vec2 vUv;
-    
+
     void main() {
       gl_FragColor = vec4(color, alpha);
     }
