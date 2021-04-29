1d5f1eaebfdaade72860d59657b0b70c37c477e9
Krzysztof Kucharski <krzysztofkucharski7@gmail.com>
2021-04-29 15:54:22 +0200

    Remove useCallback from ConfirmPage

--- src/js/pages/ConfirmPage.jsx
+++ src/js/pages/ConfirmPage.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {Link, Redirect, useLocation} from "react-router-dom";
 import {selectorAccountConfirm, tryConfirmAccount} from "redux/reducers/api/account";
