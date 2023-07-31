dc24f9f247d8c06c76c3499193ab7d8c35bdf06d
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2023-07-31 17:30:12 +0200

    Remove Chakra from registration page

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -2,7 +2,6 @@
 
 import { FC, useCallback } from 'react';
 
-import { Center, Flex, Heading, Text } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 import { DelayedTransition } from '@/components/chakra/DelayedTransition/DelayedTransition';
@@ -18,56 +17,50 @@ const SignUpPage = () => {
 
     const { t } = useTranslation();
 
-    const {
-        isIdle: registerIdle,
-        isLoading: registrationPending,
-        error: registerError,
-        isSuccess: registerSuccess,
-        mutate: registerMutate
-    } = register();
+    const signUpQuery = register();
 
     const attemptRegister = useCallback((formData: any) => {
         const { email, username, password } = formData;
-        registerMutate({
+        signUpQuery.mutate({
             email: email,
             username: username,
             password: password
         });
     }, []);
 
-    return <Center w={'100%'} h={'100%'}>
-        <Flex borderRadius={{ base: 0, sm: 8 }}
-            direction={'column'}
-            bg={'#2a2a2a'}
-            width={{ base: '100%', sm: '400px' }}
-            p={'40px'}
-            gap={'30px'}>
-            <Heading fontSize={'2xl'}>{t('REGISTRATION')}</Heading>
+    return <div className={'grid h-full w-full place-items-center'}>
+        <div
+            className={'flex w-full flex-col gap-[30px] rounded-none bg-[#2a2a2a] p-[40px] sm:w-[400px] sm:rounded-md'}>
+            <h1 className={'text-center text-2xl font-semibold'}>
+                {t('REGISTRATION')}
+            </h1>
 
             <Form<RegistrationFormArgs>
                 config={registrationForm}
                 submitButtonTextKey={'SIGN_UP'}
-                error={registerError?.data}
-                disabled={registrationPending}
+                error={signUpQuery.error?.data}
+                disabled={signUpQuery.isLoading}
                 useCancelButton={false}
                 onSubmit={attemptRegister}/>
 
             <div className={'flex flex-col items-center justify-center gap-[20px]'}>
-                <Text fontSize={'md'} color={'#C7C7C7'}>{t('ALREADY_HAVE_ACCOUNT')}</Text>
+                <div className={'text-[#C7C7C7]'}>
+                    {t('ALREADY_HAVE_ACCOUNT')}
+                </div>
                 <NavLink className={'font-semibold text-[#77a4df]'} href={'/'}>
                     {t('SIGN_IN')}
                 </NavLink>
             </div>
 
             <UserAgreements/>
-            {registrationPending && <DelayedTransition
+            {signUpQuery.isLoading && <DelayedTransition
                 pending={true}
                 position={'absolute'}
                 bottom={0}
                 left={0}
                 p={0} w={'100%'}/>}
-        </Flex>
-    </Center>;
+        </div>
+    </div>;
 };
 
 export default WithAuth(SignUpPage, {
