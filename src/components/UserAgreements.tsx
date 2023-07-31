5ee21c8fc82ced7d4fd66a611b863fc061b59ee1
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2023-07-31 17:05:45 +0200

    Remove Chakra from UserAgreements.tsx

--- src/components/UserAgreements.tsx
+++ src/components/UserAgreements.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-
-import { Flex, Text } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 import { NavLink } from '@/components/NavLink/NavLink';
@@ -9,15 +6,15 @@ const UserAgreements = () => {
 
     const { t } = useTranslation();
 
-    return <Flex flexGrow={1} direction={'column'} justify={'flex-end'}>
-        <Text textAlign={'center'} fontSize={'sm'} as={'span'}>
+    return <div className={'flex grow flex-col justify-end'}>
+        <div className={'text-center text-sm'}>
             {t('USER_AGREEMENTS')}
             <NavLink className={'p-2 font-semibold text-white'}
                 href={'/userAgreement'}>
                 {t('MORE')}
             </NavLink>
-        </Text>
-    </Flex>;
+        </div>
+    </div>;
 };
 
 export default UserAgreements;
