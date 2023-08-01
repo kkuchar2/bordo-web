40d9695668684c68a8f43b7383dc45803a317194
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2023-08-01 19:30:44 +0200

    Remove Head from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 
 import { Content } from '@/Content';
 
@@ -15,9 +14,6 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: ReactNode }) {
 
     return <html lang={'en'}>
-        <Head>
-            <link rel={'apple-touch-icon'} href={'/images/pwa/icon_x256.png'}/>
-        </Head>
         <body className={`${inter.variable} font-sans`}>
             <main>
                 <Content>
