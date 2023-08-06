a91d8b1b885717ab63772d60a835e3dc65cef60c
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2023-08-06 02:40:23 +0200

    Add support for deleting account when user is signed in with Firebase either by email or Google provider

--- src/components/DialogSystem/dialogs/DeleteAccountDialog.tsx
+++ src/components/DialogSystem/dialogs/DeleteAccountDialog.tsx
@@ -1,12 +1,22 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import {
+    deleteUser, EmailAuthProvider,
+    getAuth,
+    GoogleAuthProvider, reauthenticateWithCredential, reauthenticateWithPopup
+} from '@firebase/auth';
+import { FirebaseError } from '@firebase/util';
 import { useTranslation } from 'react-i18next';
 
 import { DelayedTransition } from '@/components/DelayedTransition/DelayedTransition';
 import Form from '@/components/Forms/Form/Form';
-import { deleteAccountForm } from '@/components/Forms/formConfig';
-import { DeleteAccountFormArgs } from '@/components/Forms/formConfig.types';
+import { deleteAccountForm, emptyForm } from '@/components/Forms/formConfig';
+import { DeleteAccountFormArgs, EmptyFormArgs } from '@/components/Forms/formConfig.types';
+import { firebaseFieldErrorConvert } from '@/components/Forms/util';
+import { isFirebaseAuthEnabled, queryClient } from '@/config';
+import { initializeFirebase } from '@/firebase/firebaseApp';
 import { deleteAccount } from '@/queries/account';
+import { QueryResponseErrorData } from '@/queries/base';
 import { closeDialog } from '@/state/reducers/dialog/dialogSlice';
 import { useAppDispatch } from '@/state/store';
 
@@ -18,9 +28,61 @@ export const DeleteAccountDialog = () => {
 
     const deleteAccountQuery = deleteAccount();
 
-    const onSubmit = useCallback((formData: DeleteAccountFormArgs) => {
-        deleteAccountQuery.mutate({ ...formData });
-    }, []);
+    const app = initializeFirebase();
+    const auth = getAuth(app);
+    const firebaseUser = auth.currentUser;
+
+    const firebaseAuthEnabled = isFirebaseAuthEnabled();
+
+    const [pending, setPending] = useState(false);
+    const [firebaseError, setFirebaseError] = useState<QueryResponseErrorData | null>(null);
+
+    const isGoogleProvider = firebaseUser?.providerData[0].providerId === 'google.com';
+
+    const isPasswordProvider = firebaseUser?.providerData[0].providerId === 'password';
+
+    const onSubmit = useCallback(async (formData: DeleteAccountFormArgs) => {
+        if (!firebaseUser) return;
+        if (!firebaseUser.email) return;
+
+        const { current_password } = formData;
+
+        setPending(true);
+
+        const credential = EmailAuthProvider.credential(firebaseUser.email, current_password);
+
+        try {
+            await reauthenticateWithCredential(firebaseUser, credential);
+            await deleteUser(firebaseUser);
+            queryClient.setQueryData(['user'], null);
+            dispatch(closeDialog());
+        }
+        catch (e) {
+            const firebaseError = e as FirebaseError;
+
+            if (firebaseError.code === 'auth/wrong-password') {
+                setFirebaseError(firebaseFieldErrorConvert(firebaseError.code, 'current_password'));
+            }
+        }
+
+        setPending(false);
+
+    }, [firebaseUser, firebaseAuthEnabled]);
+
+    const onSubmitWithGoogleProvider = useCallback(async () => {
+        if (!firebaseUser) return;
+        setPending(true);
+        try {
+            await reauthenticateWithPopup(firebaseUser, new GoogleAuthProvider());
+            await deleteUser(firebaseUser);
+            queryClient.setQueryData(['user'], null);
+            dispatch(closeDialog());
+        }
+        catch (e) {
+            return;
+        }
+        setPending(false);
+    }, [firebaseUser, firebaseAuthEnabled]);
 
     const onCancelRequest = useCallback(() => {
         dispatch(closeDialog());
@@ -37,18 +99,30 @@ export const DeleteAccountDialog = () => {
             {t('DELETE_ACCOUNT_WARNING')}
         </div>
 
-        <Form<DeleteAccountFormArgs>
+        {isPasswordProvider && <Form<DeleteAccountFormArgs>
             config={deleteAccountForm}
-            error={deleteAccountQuery.error?.data}
+            error={deleteAccountQuery.error?.data || firebaseError}
             submitButtonClassName={'rounded-md bg-white/5 px-5 py-2 text-sm font-medium text-red-500 hover:bg-white/10 hover:text-red-600'}
             submitButtonTextKey={'DELETE_ACCOUNT_PERMANENTLY'}
-            disabled={deleteAccountQuery.isLoading}
+            disabled={deleteAccountQuery.isLoading || pending}
             onCancel={onCancelRequest}
             onSubmit={onSubmit}
             initialValues={{
                 current_password: '',
             }}
-        />
-        <DelayedTransition pending={deleteAccountQuery.isLoading}/>
+        />}
+
+        {isGoogleProvider && <Form<EmptyFormArgs>
+            config={emptyForm}
+            error={null}
+            submitButtonClassName={'rounded-md bg-white/5 px-5 py-2 text-sm font-medium text-red-500 hover:bg-white/10 hover:text-red-600'}
+            submitButtonTextKey={'DELETE_ACCOUNT_PERMANENTLY'}
+            disabled={pending}
+            onCancel={onCancelRequest}
+            onSubmit={onSubmitWithGoogleProvider}
+            initialValues={{}}
+        />}
+
+        <DelayedTransition pending={deleteAccountQuery.isLoading || pending} />
     </div>;
 };
